fc9884e7c1d94a627f8134adaa96c9e226bbcecf
mho59 <mho59@student.cccd.edu>
2025-11-06 13:05:25 -0800

    string

---
+++ README.md
@@ -0,0 +1,11 @@
+# this is a readme.md
+
+This is my cs220 test
+
+- line 1
+- line 2 
+- line 3
+
+# my favorite song is cowboy like me
+
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
     cout << "hello world";
     cout << "today is a good day"
+
+    string s = "abc";
+    
     return 0;
 }
